a2c559a344ff22afa270ece4671a9420a2612994
Waqas <130844309+adamsmith19899@users.noreply.github.com>
2025-01-04 16:25:37 +0500

    Update specialGames.ts

--- src/data/specialGames.ts
+++ src/data/specialGames.ts
@@ -9,7 +9,7 @@ export const specialGames: Game[] = [
     category: 'guessing',
     thumbnail: 'https://images.unsplash.com/photo-1632167764165-74a3d686e9f8?w=800&auto=format&fit=crop&q=60',
     slug: 'wordle',
-    url: 'wordle-two-wheat.vercel.app',
+    url: 'https://wordle-two-wheat.vercel.app',
     aspectRatio: '16/9'
   },
   {
